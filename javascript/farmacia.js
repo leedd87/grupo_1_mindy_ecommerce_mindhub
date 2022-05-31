45d164f167e20bd36ee2ef0427cafb6ef248f24c
Agustin Amuchástegui <aguamu24@gmail.com>
2022-05-31 16:25:19 -0300

    modificacion martes 16:25hs

--- javascript/farmacia.js
+++ javascript/farmacia.js
@@ -1,10 +1,11 @@
 let contenedorFarmacia = document.getElementById('contenedorFarmacia')
-
 let templateCard = ''
 let dataApi;
 let arrayProductos;
 
-
+//AGREGARRRRRRR------------SOLO LINEA 7
+let inputSearch = ''
+//HASTA ACA --------- 
 async function getDataApi() {
 
    await fetch('https://apipetshop.herokuapp.com/api/articulos')
@@ -28,7 +29,7 @@ async function getDataApi() {
             <h4 class="card-title mb-3">$${element.precio}</4>
                <h6 class="card-text mb-3">${element.nombre}</h6>
                <div class="d-flex justify-content-center">
-                  <a href="./details.html?id=${element._id}" class="btn btn-primary">Ver más</a>
+                  <a href="./html/detalles.html?id=${element._id}" class="btn btn-primary">Ver más</a>
                </div>
          </div>
       </div>
@@ -38,6 +39,55 @@ async function getDataApi() {
       contenedorFarmacia.innerHTML = templateCard
    })
 
+   //----------------------------AGREGAR FARMACIA DESDE LA LINEA 42---------
+   searchBar.addEventListener('keyup', (e) => {
+
+      inputSearch = e.target.value
+      console.log(inputSearch)
+      filterCards()
+   })
+
+   function filterCards() {
+      let arrayFiltros = []
+      if (inputSearch !== '') {
+         arrayFiltros.push(...arrayMedicamentos.filter(element => element.nombre.toLowerCase().includes(inputSearch.trim().toLowerCase())))
+
+      } else {
+         arrayFiltros.push(...arrayMedicamentos)
+      }
+      crearCards(arrayFiltros)
+
+   }
+
+   filterCards()
+   //HASTA ACA DENTRO DE LA ASYNC
 }
 
 getDataApi()
+
+//-----------AGREGARRRRRRRRRR ESTO POR FUERA DE LA ASYNC
+function crearCards(array) {
+   let templateCard = ''
+   array.forEach(element => {
+
+      templateCard += `
+      <div class="col-lg-3 col-md-6 col-sm-12 my-2">
+      <div class="card">
+      <img
+      src="${element.imagen}"
+      class="card-img-top border" alt="${element.nombre}">
+      <div class="card-body">
+      ${element.stock < 5 ? `<p class="ultimas-unidades"><small>ULTIMAS UNIDADES</small></p>` : `<p>STOCK: ${element.stock}</p>`}
+      <h4 class="card-title mb-3">$${element.precio}</4>
+      <h6 class="card-text mb-3">${element.nombre}</h6>
+      <div class="d-flex justify-content-center">
+      <a href="./details.html?id=${element._id}" class="btn btn-primary">Ver más</a>
+      </div>
+      </div>
+      </div>
+      </div>
+      
+      `
+      contenedorFarmacia.innerHTML = templateCard
+   })
+}
